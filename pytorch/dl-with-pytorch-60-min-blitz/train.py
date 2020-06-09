3efe58a0c51b678fca3721b646b210a8eb5f6692
Yosuke <yyajima@gatech.edu>
2020-06-09 11:28:35 -0400

    Update code

--- pytorch/dl-with-pytorch-60-min-blitz/train.py
+++ pytorch/dl-with-pytorch-60-min-blitz/train.py
@@ -9,26 +9,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 import torch.optim as optim
-
-# convolutional neural network
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+import model.model
 
 # functions to show an image
 def imshow(img):
@@ -63,7 +44,8 @@ imshow(torchvision.utils.make_grid(images))
 # print labels
 print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
-net = Net()
+net = model.model.Net()
+print(net)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
