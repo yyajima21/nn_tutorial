1adf36c17d45e4c63277aca48945a1e77d1e9d8e
Yosuke <yyajima@gatech.edu>
2020-06-10 11:05:39 -0400

    Created a train function

--- pytorch/dl-with-pytorch-60-min-blitz/train.py
+++ pytorch/dl-with-pytorch-60-min-blitz/train.py
@@ -20,6 +20,28 @@ def imshow(img):
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
+def train(args, net, device, trainloader, criterion, optimizer, epoch):
+    running_loss = 0.0
+    for i, (data, target) in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data.to(device), target.to(device)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
+            
+            running_loss = 0.0
+
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch Tutorial Example')
@@ -76,27 +98,7 @@ def main():
 
     # Training code
     for epoch in range(2):  # loop over the dataset multiple times
-
-        running_loss = 0.0
-        for i, (data, target) in enumerate(trainloader, 0):
-            # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data.to(device), target.to(device)
-
-            # zero the parameter gradients
-            optimizer.zero_grad()
-
-            # forward + backward + optimize
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-
-            # print statistics
-            running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                    (epoch + 1, i + 1, running_loss / 2000))
-                running_loss = 0.0
+        train(args, net, device, trainloader, criterion, optimizer, epoch)
 
     print('Finished Training')
 
