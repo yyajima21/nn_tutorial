d58252fe71c3b5a87def04b5b280d93b2d108fd0
Yosuke <yyajima@gatech.edu>
2020-06-22 10:54:49 -0400

    Added a pc curve

--- pytorch/visualizing-models-data-and-training-with-tensorboard/train.py
+++ pytorch/visualizing-models-data-and-training-with-tensorboard/train.py
@@ -73,6 +73,21 @@ def plot_classes_preds(net, images, labels, classes):
                     color=("green" if preds[idx]==labels[idx].item() else "red"))
     return fig
 
+# helper function
+def add_pr_curve_tensorboard(class_index, test_probs, test_preds, global_step=0):
+    '''
+    Takes in a "class_index" from 0 to 9 and plots the corresponding
+    precision-recall curve
+    '''
+    tensorboard_preds = test_preds == class_index
+    tensorboard_probs = test_probs[:, class_index]
+
+    writer.add_pr_curve(classes[class_index],
+                        tensorboard_preds,
+                        tensorboard_probs,
+                        global_step=global_step)
+    writer.close()
+
 def main():
     # transforms
     transform = transforms.Compose(
@@ -169,8 +184,31 @@ def main():
                                 plot_classes_preds(net, inputs, labels, classes),
                                 global_step=epoch * len(trainloader) + i)
                 running_loss = 0.0
-    writer.close()
+    #writer.close()
     print('Finished Training')
+
+    # 1. gets the probability predictions in a test_size x num_classes Tensor
+    # 2. gets the preds in a test_size Tensor
+    # takes ~10 seconds to run
+    class_probs = []
+    class_preds = []
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            output = net(images)
+            class_probs_batch = [F.softmax(el, dim=0) for el in output]
+            _, class_preds_batch = torch.max(output, 1)
+
+            class_probs.append(class_probs_batch)
+            class_preds.append(class_preds_batch)
+
+    test_probs = torch.cat([torch.stack(batch) for batch in class_probs])
+    test_preds = torch.cat(class_preds)
+
+    # plot all the pr curves
+    for i in range(len(classes)):
+        add_pr_curve_tensorboard(i, test_probs, test_preds)
+    writer.close()
 
 if __name__ == '__main__':
     main()
