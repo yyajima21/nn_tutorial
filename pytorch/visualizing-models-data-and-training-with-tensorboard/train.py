7a72b6599c1e5bf7214c4473920e9da11e136ba1
Yosuke <yyajima@gatech.edu>
2020-06-23 10:38:20 -0400

    Refactor code

--- pytorch/visualizing-models-data-and-training-with-tensorboard/train.py
+++ pytorch/visualizing-models-data-and-training-with-tensorboard/train.py
@@ -1,4 +1,7 @@
 # imports
+from __future__ import print_function
+import argparse
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -10,26 +13,46 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import model.model
+
+import utils
+
 # tensorboard
 from torch.utils.tensorboard import SummaryWriter
 
+# tensorboard (Google Colab)
 import tensorflow as tf
 import tensorboard as tb
 tf.io.gfile = tb.compat.tensorflow_stub.io.gfile
 
-# helper function to show an image
-# (used in the `plot_classes_preds` function below)
-def matplotlib_imshow(img, one_channel=False):
-    if one_channel:
-        img = img.mean(dim=0)
-    img = img / 2 + 0.5     # unnormalize
-    npimg = img.numpy()
-    if one_channel:
-        plt.imshow(npimg, cmap="Greys")
-    else:
-        plt.imshow(np.transpose(npimg, (1, 2, 0)))
+# constant for classes
+classes = ('T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+            'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot')
 
-# helper function
+def parse_args():
+    # Training settings
+    parser = argparse.ArgumentParser(description='Tensorboard Tutorial Example')
+    parser.add_argument('--train-batch-size', type=int, default=4, metavar='N',
+                        help='input batch size for training (default: 4)')
+    parser.add_argument('--test-batch-size', type=int, default=4, metavar='N',
+                        help='input batch size for testing (default: 4)')
+    parser.add_argument('--epochs', type=int, default=2, metavar='N',
+                        help='number of epochs to train (default: 2)')
+    parser.add_argument('--num_workers', type=int, default=2, metavar='N',
+                        help='number of num workers (default: 2)')
+    parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
+                        help='learning rate (default: 0.001)')
+    parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
+                        help='momentum (default: 0.9)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--save-model', action='store_true', default=False,
+                        help='For Saving the current Model')
+    return parser.parse_args()
+
 def select_n_random(data, labels, n=100):
     '''
     Selects n random datapoints and their corresponding labels from a dataset
@@ -38,8 +61,6 @@ def select_n_random(data, labels, n=100):
 
     perm = torch.randperm(len(data))
     return data[perm][:n], labels[perm][:n]
-
-# helper functions
 
 def images_to_probs(net, images):
     '''
@@ -52,7 +73,7 @@ def images_to_probs(net, images):
     preds = np.squeeze(preds_tensor.numpy())
     return preds, [F.softmax(el, dim=0)[i].item() for i, el in zip(preds, output)]
 
-def plot_classes_preds(net, images, labels, classes):
+def plot_classes_preds(net, images, labels):
     '''
     Generates matplotlib Figure using a trained network, along with images
     and labels from a batch, that shows the network's top prediction along
@@ -73,8 +94,33 @@ def plot_classes_preds(net, images, labels, classes):
                     color=("green" if preds[idx]==labels[idx].item() else "red"))
     return fig
 
-# helper function
-def add_pr_curve_tensorboard(class_index, test_probs, test_preds, writer, classes, global_step=0):
+def train(args, net, device, trainloader, criterion, optimizer, epoch):
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+        # zero the parameter gradients
+        optimizer.zero_grad()
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item()
+        if i % 1000 == 999:    # every 1000 mini-batches...
+            print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 1000))
+            # ...log the running loss
+            writer.add_scalar('training loss',
+                                running_loss / 1000,
+                                epoch * len(trainloader) + i)
+            # ...log a Matplotlib Figure showing the model's predictions on a
+            # random mini-batch
+            writer.add_figure('predictions vs. actuals', plot_classes_preds(net, inputs, labels), global_step=epoch * len(trainloader) + i)
+            running_loss = 0.0
+    print('Finished Training')
+
+def add_pr_curve_tensorboard(class_index, test_probs, test_preds, writer, global_step=0):
     '''
     Takes in a "class_index" from 0 to 9 and plots the corresponding
     precision-recall curve
@@ -86,9 +132,18 @@ def add_pr_curve_tensorboard(class_index, test_probs, test_preds, writer, classe
                         tensorboard_preds,
                         tensorboard_probs,
                         global_step=global_step)
-    writer.close()
 
 def main():
+    args = parse_args()
+
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+
+    torch.manual_seed(args.seed)
+
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    kwargs = {'num_workers': 2, 'pin_memory': True} if use_cuda else {}
+
     # transforms
     transform = transforms.Compose(
         [transforms.ToTensor(),
@@ -105,20 +160,16 @@ def main():
         transform=transform)
 
     # dataloaders
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                            shuffle=True, num_workers=2)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.train_batch_size,
+                                            shuffle=True, num_workers=args.num_workers)
 
-    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                            shuffle=False, num_workers=2)
-
-    # constant for classes
-    classes = ('T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-            'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot')
+    testloader = torch.utils.data.DataLoader(testset, batch_size=args.test_batch_size,
+                                            shuffle=False, num_workers=args.num_workers)
     
     net = model.model.Net()
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=args.momentum)
 
     # default `log_dir` is "runs" - we'll be more specific here
     writer = SummaryWriter('runs/fashion_mnist_experiment_1')
@@ -130,15 +181,13 @@ def main():
     # create grid of images
     img_grid = torchvision.utils.make_grid(images)
 
-    # show images
-    matplotlib_imshow(img_grid, one_channel=True)
-
+    # show images (verify data image)
+    #utils.matplotlib_imshow(img_grid, one_channel=True)
+    
     # write to tensorboard
     writer.add_image('four_fashion_mnist_images', img_grid)
 
     writer.add_graph(net, images)
-    
-    #writer.close()
 
     # select random images and their target indices
     images, labels = select_n_random(trainset.data, trainset.targets)
@@ -151,41 +200,9 @@ def main():
     writer.add_embedding(features,
                         metadata=class_labels,
                         label_img=images.unsqueeze(1))
-    #writer.close()
 
-    running_loss = 0.0
-    for epoch in range(1):  # loop over the dataset multiple times
-
-        for i, data in enumerate(trainloader, 0):
-
-            # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data
-
-            # zero the parameter gradients
-            optimizer.zero_grad()
-
-            # forward + backward + optimize
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-
-            running_loss += loss.item()
-            if i % 1000 == 999:    # every 1000 mini-batches...
-
-                # ...log the running loss
-                writer.add_scalar('training loss',
-                                running_loss / 1000,
-                                epoch * len(trainloader) + i)
-
-                # ...log a Matplotlib Figure showing the model's predictions on a
-                # random mini-batch
-                writer.add_figure('predictions vs. actuals',
-                                plot_classes_preds(net, inputs, labels, classes),
-                                global_step=epoch * len(trainloader) + i)
-                running_loss = 0.0
-    #writer.close()
-    print('Finished Training')
+    for epoch in range(args.epocjs):
+        train(args, net, device, trainloader, criterion, optimizer, epoch)
 
     # 1. gets the probability predictions in a test_size x num_classes Tensor
     # 2. gets the preds in a test_size Tensor
@@ -207,7 +224,9 @@ def main():
 
     # plot all the pr curves
     for i in range(len(classes)):
-        add_pr_curve_tensorboard(i, test_probs, test_preds, writer, classes)
-
+        add_pr_curve_tensorboard(i, test_probs, test_preds, writer)
+    
+    writer.close()
+    
 if __name__ == '__main__':
     main()
