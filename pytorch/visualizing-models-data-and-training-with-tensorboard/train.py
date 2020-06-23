b05e9a44aeb20d8e3caf7907952db4708a71de37
Yosuke <yyajima@gatech.edu>
2020-06-23 11:36:47 -0400

    Fix typo

--- pytorch/visualizing-models-data-and-training-with-tensorboard/train.py
+++ pytorch/visualizing-models-data-and-training-with-tensorboard/train.py
@@ -121,6 +121,30 @@ def train(args, net, device, trainloader, criterion, optimizer, epoch, writer):
     print('Finished Training')
     return writer
 
+def test(net, device, testloader, writer):
+    # 1. gets the probability predictions in a test_size x num_classes Tensor
+    # 2. gets the preds in a test_size Tensor
+    # takes ~10 seconds to run
+    class_probs = []
+    class_preds = []
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            output = net(images)
+            class_probs_batch = [F.softmax(el, dim=0) for el in output]
+            _, class_preds_batch = torch.max(output, 1)
+
+            class_probs.append(class_probs_batch)
+            class_preds.append(class_preds_batch)
+
+    test_probs = torch.cat([torch.stack(batch) for batch in class_probs])
+    test_preds = torch.cat(class_preds)
+
+    # plot all the pr curves
+    for i in range(len(classes)):
+        writer = add_pr_curve_tensorboard(i, test_probs, test_preds, writer)
+    return writer
+
 def add_pr_curve_tensorboard(class_index, test_probs, test_preds, writer, global_step=0):
     '''
     Takes in a "class_index" from 0 to 9 and plots the corresponding
@@ -204,7 +228,7 @@ def main():
 
     for epoch in range(args.epochs):
         writer = train(args, net, device, trainloader, criterion, optimizer, epoch, writer)
-
+    """
     # 1. gets the probability predictions in a test_size x num_classes Tensor
     # 2. gets the preds in a test_size Tensor
     # takes ~10 seconds to run
@@ -226,7 +250,7 @@ def main():
     # plot all the pr curves
     for i in range(len(classes)):
         add_pr_curve_tensorboard(i, test_probs, test_preds, writer)
-    
+    """
     writer.close()
     
 if __name__ == '__main__':
