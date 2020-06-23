c5fbee0c8d8997712c9a26c26dfcefeb2ae424d8
Yosuke <yyajima@gatech.edu>
2020-06-23 10:52:07 -0400

    Fix typo

--- pytorch/visualizing-models-data-and-training-with-tensorboard/train.py
+++ pytorch/visualizing-models-data-and-training-with-tensorboard/train.py
@@ -86,7 +86,7 @@ def plot_classes_preds(net, images, labels):
     fig = plt.figure(figsize=(12, 48))
     for idx in np.arange(4):
         ax = fig.add_subplot(1, 4, idx+1, xticks=[], yticks=[])
-        matplotlib_imshow(images[idx], one_channel=True)
+        utils.matplotlib_imshow(images[idx], one_channel=True)
         ax.set_title("{0}, {1:.1f}%\n(label: {2})".format(
             classes[preds[idx]],
             probs[idx] * 100.0,
